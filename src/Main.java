76f0084c0f2726bc0ed65ca1fa8c47081177ea61
Sandra López Iglesias <slopeziglesias@danielcastelao.org>
2023-05-30 20:36:41 +0200

    clase controller

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import com.huerto.controller.HuertoController;
 import com.huerto.view.HuertoCatalogoIU;
 import com.huerto.view.HuertoIU;
 import com.huerto.view.HuertoPersonalizadoIU;
@@ -6,12 +7,15 @@ public class Main {
     public static void main(String[] args) {
 
         HuertoIU huertoIU= new HuertoIU();
-        huertoIU.setVisible(true);
+        // huertoIU.setVisible(true);
 
         HuertoCatalogoIU huertoCatalogoIU= new HuertoCatalogoIU();
-        huertoCatalogoIU.setVisible(true);
+        // huertoCatalogoIU.setVisible(true);
 
         HuertoPersonalizadoIU huertoPersonalizadoIU= new HuertoPersonalizadoIU();
-        huertoPersonalizadoIU.setVisible(true);
+        // huertoPersonalizadoIU.setVisible(true);
+
+        HuertoController controller = new HuertoController(huertoIU, huertoCatalogoIU, huertoPersonalizadoIU);
+        huertoIU.setVisible(true);
     }
 }
