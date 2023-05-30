9ed44d29d99c0f9e07060e236e9321e5af1af95a
Lucía Balsa <113551587+LuciaB16@users.noreply.github.com>
2023-05-30 11:40:54 +0200

    Iu (#4)
    * Creación IU
    
    * Actualización IU
    
    * Añadido de métodos en IU

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import com.huerto.view.HuertoCatalogoIU;
+import com.huerto.view.HuertoIU;
+import com.huerto.view.HuertoPersonalizadoIU;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HuertoIU huertoIU= new HuertoIU();
+        huertoIU.setVisible(true);
+
+        HuertoCatalogoIU huertoCatalogoIU= new HuertoCatalogoIU();
+        huertoCatalogoIU.setVisible(true);
+
+        HuertoPersonalizadoIU huertoPersonalizadoIU= new HuertoPersonalizadoIU();
+        huertoPersonalizadoIU.setVisible(true);
     }
 }
